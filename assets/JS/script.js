3d7ac6db7efbec9f03dce9831139e7914125960e
Randie <srandall1213@gmail.com>
2022-07-11 13:47:13 -0600

    local storage & render is working at 9AM

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,21 @@
-var currentDayEl = document.querySelector("#currentDay");
-
 //Today's Date
+var currentDayEl = document.getElementById("currentDay");
 currentDayEl.textContent = moment().format('dddd') + ", " +  moment().format("MMM Do");   
 
+//Save Event
+var saveBtn = document.getElementById("save");
+
+function renderEvent() {
+   var myEvent = localStorage.getItem("My Event:");
+   var descriptionEl = document.getElementById("description");
+   descriptionEl.textContent = myEvent;
+}
+
+saveBtn.addEventListener("click", function(event) {
+   event.preventDefault();
+   var descriptionEl = document.getElementById("description").value;
+   localStorage.setItem("My Event:", descriptionEl);
+   
+});
+renderEvent();
 
